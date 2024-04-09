47df7601f7728afdfa45ec437efa2823d4eb89e8
Jean <jean.viaene@epfl.ch>
2024-04-09 12:09:15 +0200

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -f main.zip go-imports
-GOOS=linux GOARCH=amd64 GO111MODULE=on go build
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=auto go build
 zip -X -r ./main.zip go-imports
 
 aws lambda update-function-code --function-name dedis-go-imports --zip-file fileb://main.zip
